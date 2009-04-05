8c69919e5463d7446dc8a52edff7f406be7231e9
p3lim-52096 <p3lim-52096@ae24c21f-2f0c-4c94-9256-0092abe71e0d>
2009-04-05 23:06:49 +0000

    Added Ace3 and SharedMedia as optionaldeps Removed the clock option from blizzard Fixed the calendar invite "popup" Fixed the savedvariable handling
    git-svn-id: svn://svn.wowinterface.com/pMinimap-54/trunk@88 ae24c21f-2f0c-4c94-9256-0092abe71e0d

--- pMinimap/pMinimap.lua
+++ pMinimap/pMinimap.lua
@@ -2,9 +2,6 @@ pMinimap = CreateFrame('Frame', 'pMinimap', UIParent)
 pMinimap:SetScript('OnEvent', function(self, event, ...) self[event](self, event, ...) end)
 pMinimap:RegisterEvent('ADDON_LOADED')
 
-local dummy = CreateFrame('Frame', nil, InterfaceOptionsFrame)
-dummy:SetScript('OnShow', function(self) if(not IsAddOnLoaded('pMinimap_Config')) then LoadAddOn('pMinimap_Config') end self:SetScript('OnShow', nil) end)
-
 local LSM = LibStub('LibSharedMedia-3.0')
 
 local onUpdate, onClickClock, onClickCoord, onMouseWheel
@@ -79,34 +76,10 @@ do
 end
 
 
-local function DisableBlizzard()
-	for k,v in next, {InterfaceOptionsDisplayPanelShowClock} do
-		local warning = v:CreateFontString(nil, 'OVERLAY', 'GameFontNormal')
-		warning:SetPoint('TOPLEFT', v, 0, 10)
-		warning:SetText('|cff00ff33OVERRID BY PMINIMAP!|r')
-
-		v:SetButtonState('DISABLED', true)
-	end
-
-	InterfaceOptionsDisplayPanelShowClock.setFunc('1')
-	InterfaceOptionsDisplayPanelShowClock.setFunc = function() end
-end
-
-local function LoadDefaults()
-	pMinimapDB = pMinimapDB or {}
-	for k,v in pairs(defaults) do
-		if(type(pMinimapDB[k]) == 'nil') then
-			pMinimapDB[k] = v
-		end
-	end
-
-	pMinimapDB.unlocked = false
-end
-
-local function SlashCommand(str)
+local function slashHandler(str)
 	if(str == 'reset') then
-		pMinimapDB = {}
-		LoadDefaults()
+		pMinimapDB = nil
+		pMinimap:LoadDefaults()
 		print('|cffff8080pMinimap:|r Savedvariables is now reset. You should reload/relog to affect changes.')
 	elseif(str == 'refresh') then
 		Minimap:SetMaskTexture([=[Interface\ChatFrame\ChatFrameBackground]=])
@@ -119,20 +92,132 @@ local function SlashCommand(str)
 	end
 end
 
+local function optionsPanel(self)
+	if(not IsAddOnLoaded('pMinimap_Config')) then
+		LoadAddOn('pMinimap_Config')
+	end
+	self:SetScript('OnShow', nil)
 
-local function CreateClock(self)
+	InterfaceOptionsDisplayPanelShowClock:Disable()
+	InterfaceOptionsDisplayPanelShowClock:Hide()
+end
+
+local function Initialize(self)
+	MinimapZoomIn:Hide()
+	MinimapZoomOut:Hide()
+
+	MiniMapTrackingBackground:Hide()
+	MiniMapTrackingButton:SetHighlightTexture('')
+	MiniMapTrackingButtonBorder:SetTexture('')
+	MiniMapTrackingIcon:SetTexCoord(0.065, 0.935, 0.065, 0.935)
+	MiniMapTrackingIconOverlay:SetTexture('')
+	MiniMapTracking:SetParent(Minimap)
+	MiniMapTracking:ClearAllPoints()
+	MiniMapTracking:SetPoint('TOPLEFT', -2, 2)
+
+	BattlegroundShine:Hide()
+	MiniMapBattlefieldBorder:SetTexture('')
+	MiniMapBattlefieldFrame:SetParent(Minimap)
+	MiniMapBattlefieldFrame:ClearAllPoints()
+	MiniMapBattlefieldFrame:SetPoint('TOPRIGHT', -2, -2)
+
+	MiniMapMailBorder:SetTexture('')
+	MiniMapMailFrame:SetParent(Minimap)
+	MiniMapMailFrame:ClearAllPoints()
+	MiniMapMailFrame:SetPoint('TOP', 0, -4)
+	MiniMapMailFrame:SetHeight(8)
+
+	MiniMapMailText = MiniMapMailFrame:CreateFontString(nil, 'OVERLAY')
+	MiniMapMailText:SetFont(LSM:Fetch('font', self.db.smfont), self.db.fontsize, self.db.fontflag)
+	MiniMapMailText:SetPoint('BOTTOM', 0, 2)
+	MiniMapMailText:SetText('New Mail!')
+	MiniMapMailText:SetTextColor(1, 1, 1)
+
+	MinimapZoneTextButton:SetParent(Minimap)
+	MinimapZoneTextButton:ClearAllPoints()
+	MinimapZoneTextButton:SetPoint(self.db.zonePoint == 'BOTTOM' and 'TOP' or 'BOTTOM', Minimap, self.db.zonePoint, 0, self.db.zoneOffset)
+	MinimapZoneTextButton:SetWidth(Minimap:GetWidth() * 1.5)
+
+	MinimapZoneText:ClearAllPoints()
+	MinimapZoneText:SetAllPoints(MinimapZoneTextButton)
+	MinimapZoneText:SetFont(LSM:Fetch('font', self.db.smfont), self.db.fontsize, self.db.fontflag)
+	MinimapZoneText:SetShadowOffset(0, 0)
+
+	MinimapBorder:SetTexture('')
+	MinimapBorderTop:Hide()
+	MinimapToggleButton:Hide()
+
+	GameTimeFrame:Hide()
+	MiniMapWorldMapButton:Hide()
+	MiniMapMeetingStoneFrame:SetAlpha(0)
+	MiniMapVoiceChatFrame:Hide()
+	MiniMapVoiceChatFrame.Show = MiniMapVoiceChatFrame.Hide
+	MinimapNorthTag:SetAlpha(0)
+
+	Minimap:EnableMouseWheel()
+	Minimap:SetScript('OnMouseWheel', onMouseWheel)
+	Minimap:SetScale(self.db.scale)
+	Minimap:SetFrameLevel(self.db.level)
+	Minimap:SetFrameStrata(self.db.strata)
+	Minimap:SetMaskTexture([=[Interface\ChatFrame\ChatFrameBackground]=])
+	Minimap:SetBackdrop({bgFile = [=[Interface\ChatFrame\ChatFrameBackground]=], insets = {top = - self.db.offset, left = - self.db.offset, bottom = - self.db.offset, right = - self.db.offset}})
+	Minimap:SetBackdropColor(unpack(self.db.colors))
+
+	MinimapCluster:EnableMouse(false)
+	Minimap:SetMovable(true)
+	Minimap:RegisterForDrag('LeftButton')
+	Minimap:SetScript('OnDragStop', function() if(pMinimapDB.unlocked) then Minimap:StopMovingOrSizing() end end)
+	Minimap:SetScript('OnDragStart', function() if(pMinimapDB.unlocked) then Minimap:StartMoving() end end)
+
+	if(not self.db.zone) then
+		MinimapZoneTextButton:Hide()
+	end
+
+	if(self.db.dura) then
+		DurabilityFrame:SetAlpha(0)
+
+		self:RegisterEvent('UPDATE_INVENTORY_ALERTS')
+		self.UPDATE_INVENTORY_ALERTS()
+	end
+
+	if(self.db.coords) then
+		self:CreateCoords()
+	end
+
+	if(self.db.clock) then
+		self:CreateClock()
+	else
+		TimeManagerClockButton:Hide()
+	end
+
+	if(self.db.mail) then
+		MiniMapMailIcon:Hide()
+	else
+		MiniMapMailText:Hide()
+	end
+end
+
+
+function pMinimap:LoadDefaults()
+	pMinimapDB = setmetatable(pMinimapDB or {}, {__index = defaults})
+	pMinimapDB.unlocked = false
+
+	self.db = pMinimapDB
+end
+
+function pMinimap:CreateClock()
 	TimeManager_LoadUI()
 
 	TimeManagerClockButton:SetWidth(40)
 	TimeManagerClockButton:SetHeight(14)
 	TimeManagerClockButton:ClearAllPoints()
-	TimeManagerClockButton:SetPoint(pMinimapDB.coords and 'BOTTOMLEFT' or 'BOTTOM', Minimap)
+	TimeManagerClockButton:SetPoint(self.db.coords and 'BOTTOMLEFT' or 'BOTTOM', Minimap)
 	TimeManagerClockButton:GetRegions():Hide()
 	TimeManagerClockButton:Show()
 	TimeManagerClockButton:SetScript('OnClick', onClickClock)
 
 	TimeManagerClockTicker:SetPoint('CENTER', TimeManagerClockButton)
-	TimeManagerClockTicker:SetFont(LSM:Fetch('font', pMinimapDB.smfont), pMinimapDB.fontsize, pMinimapDB.fontflag)
+	TimeManagerClockTicker:SetFont(LSM:Fetch('font', self.db.smfont), self.db.fontsize, self.db.fontflag)
 	TimeManagerClockTicker:SetShadowOffset(0, 0)
 
 	TimeManagerAlarmFiredTexture.Show = function() TimeManagerClockTicker:SetTextColor(1, 0, 0) end
@@ -144,23 +229,43 @@ local function CreateClock(self)
 	self.RunClock = true
 end
 
-local function CreateCoords(self)
+function pMinimap:CreateCoords()
 	self.Coord = CreateFrame('Button', nil, Minimap)
-	self.Coord:SetPoint(pMinimapDB.clock and 'BOTTOMRIGHT' or 'BOTTOM', Minimap)
+	self.Coord:SetPoint(self.db.clock and 'BOTTOMRIGHT' or 'BOTTOM', Minimap)
 	self.Coord:SetWidth(40)
 	self.Coord:SetHeight(14)
 	self.Coord:RegisterForClicks('AnyUp')
 
 	self.Coord.Text = self.Coord:CreateFontString(nil, 'OVERLAY')
 	self.Coord.Text:SetPoint('CENTER', self.Coord)
-	self.Coord.Text:SetFont(LSM:Fetch('font', pMinimapDB.smfont), pMinimapDB.fontsize, pMinimapDB.fontflag)
+	self.Coord.Text:SetFont(LSM:Fetch('font', self.db.smfont), self.db.fontsize, self.db.fontflag)
 	self.Coord.Text:SetTextColor(1, 1, 1)
 
 	self.Coord:SetScript('OnClick', onClickCoord)
 	self.Coord:SetScript('OnUpdate', onUpdate)
 
 	self:RegisterEvent('ZONE_CHANGED_NEW_AREA')
-	self.RunCoords = true
+end
+
+
+function pMinimap:ADDON_LOADED(event, addon)
+	if(addon ~= 'pMinimap') then return end
+	self:UnregisterEvent(event)
+
+	CreateFrame('Frame', nil, InterfaceOptionsFrame):SetScript('OnShow', optionsPanel)
+	LSM:Register('font', 'Visitor TT1', [=[Interface\AddOns\pMinimap\font.ttf]=])
+
+	SLASH_PMINIMAP1 = '/pmm'
+	SLASH_PMINIMAP2 = '/pminimap'
+	SlashCmdList.PMINIMAP = slashHandler
+
+	self:LoadDefaults()
+	self.onCoordUpdate = onUpdate
+
+	InterfaceOptionsDisplayPanelShowClock.setFunc('1')
+	InterfaceOptionsDisplayPanelShowClock.setFunc = function() end
+
+	Initialize(self)
 end
 
 function pMinimap:CALENDAR_UPDATE_PENDING_INVITES()
@@ -190,121 +295,6 @@ function pMinimap:UPDATE_INVENTORY_ALERTS()
 	else
 		Minimap:SetBackdropColor(unpack(pMinimapDB.colors))
 	end
-end
-
-local function Initialize(self)
-	MinimapZoomIn:Hide()
-	MinimapZoomOut:Hide()
-
-	MiniMapTrackingBackground:Hide()
-	MiniMapTrackingButton:SetHighlightTexture('')
-	MiniMapTrackingButtonBorder:SetTexture('')
-	MiniMapTrackingIcon:SetTexCoord(0.065, 0.935, 0.065, 0.935)
-	MiniMapTrackingIconOverlay:SetTexture('')
-	MiniMapTracking:SetParent(Minimap)
-	MiniMapTracking:ClearAllPoints()
-	MiniMapTracking:SetPoint('TOPLEFT', -2, 2)
-
-	BattlegroundShine:Hide()
-	MiniMapBattlefieldBorder:SetTexture('')
-	MiniMapBattlefieldFrame:SetParent(Minimap)
-	MiniMapBattlefieldFrame:ClearAllPoints()
-	MiniMapBattlefieldFrame:SetPoint('TOPRIGHT', -2, -2)
-
-	MiniMapMailBorder:SetTexture('')
-	MiniMapMailFrame:SetParent(Minimap)
-	MiniMapMailFrame:ClearAllPoints()
-	MiniMapMailFrame:SetPoint('TOP', 0, -4)
-	MiniMapMailFrame:SetHeight(8)
-
-	MiniMapMailText = MiniMapMailFrame:CreateFontString(nil, 'OVERLAY')
-	MiniMapMailText:SetFont(LSM:Fetch('font', pMinimapDB.smfont), pMinimapDB.fontsize, pMinimapDB.fontflag)
-	MiniMapMailText:SetPoint('BOTTOM', 0, 2)
-	MiniMapMailText:SetText('New Mail!')
-	MiniMapMailText:SetTextColor(1, 1, 1)
-
-	MinimapZoneTextButton:SetParent(Minimap)
-	MinimapZoneTextButton:ClearAllPoints()
-	MinimapZoneTextButton:SetPoint(pMinimapDB.zonePoint == 'BOTTOM' and 'TOP' or 'BOTTOM', Minimap, pMinimapDB.zonePoint, 0, pMinimapDB.zoneOffset)
-	MinimapZoneTextButton:SetWidth(Minimap:GetWidth() * 1.5)
-
-	MinimapZoneText:ClearAllPoints()
-	MinimapZoneText:SetAllPoints(MinimapZoneTextButton)
-	MinimapZoneText:SetFont(LSM:Fetch('font', pMinimapDB.smfont), pMinimapDB.fontsize, pMinimapDB.fontflag)
-	MinimapZoneText:SetShadowOffset(0, 0)
-
-	MinimapBorder:SetTexture('')
-	MinimapBorderTop:Hide()
-	MinimapToggleButton:Hide()
-
-	GameTimeFrame:Hide()
-	MiniMapWorldMapButton:Hide()
-	MiniMapMeetingStoneFrame:SetAlpha(0)
-	MiniMapVoiceChatFrame:Hide()
-	MiniMapVoiceChatFrame.Show = MiniMapVoiceChatFrame.Hide
-	MinimapNorthTag:SetAlpha(0)
-
-	Minimap:EnableMouseWheel()
-	Minimap:SetScript('OnMouseWheel', onMouseWheel)
-	Minimap:SetScale(pMinimapDB.scale)
-	Minimap:SetFrameLevel(pMinimapDB.level)
-	Minimap:SetFrameStrata(pMinimapDB.strata)
-	Minimap:SetMaskTexture([=[Interface\ChatFrame\ChatFrameBackground]=])
-	Minimap:SetBackdrop({bgFile = [=[Interface\ChatFrame\ChatFrameBackground]=], insets = {top = - pMinimapDB.offset, left = - pMinimapDB.offset, bottom = - pMinimapDB.offset, right = - pMinimapDB.offset}})
-	Minimap:SetBackdropColor(unpack(pMinimapDB.colors))
-
-	MinimapCluster:EnableMouse(false)
-	Minimap:SetMovable(true)
-	Minimap:RegisterForDrag('LeftButton')
-	Minimap:SetScript('OnDragStop', function() if(pMinimapDB.unlocked) then Minimap:StopMovingOrSizing() end end)
-	Minimap:SetScript('OnDragStart', function() if(pMinimapDB.unlocked) then Minimap:StartMoving() end end)
-
-	if(not pMinimapDB.zone) then
-		MinimapZoneTextButton:Hide()
-	end
-
-	if(pMinimapDB.dura) then
-		DurabilityFrame:SetAlpha(0)
-
-		self:RegisterEvent('UPDATE_INVENTORY_ALERTS')
-		self.UPDATE_INVENTORY_ALERTS()
-	end
-
-	if(pMinimapDB.coords) then
-		CreateCoords(self)
-	end
-
-	if(pMinimapDB.clock) then
-		CreateClock(self)
-	else
-		TimeManagerClockButton:Hide()
-	end
-
-	if(pMinimapDB.mail) then
-		MiniMapMailIcon:Hide()
-	else
-		MiniMapMailText:Hide()
-	end
-end
-
-
-function pMinimap:ADDON_LOADED(event, addon)
-	if(addon ~= 'pMinimap') then return end
-
-	self.onUpdate = onUpdate
-	self:UnregisterEvent(event)	
-	DisableBlizzard()
-
-	SLASH_PMINIMAP1 = '/pmm'
-	SLASH_PMINIMAP2 = '/pminimap'
-	SlashCmdList.PMINIMAP = SlashCommand
-
-	LSM:Register('font', 'Visitor TT1', [=[Interface\AddOns\pMinimap\font.ttf]=])
-	LoadDefaults()
-	Initialize(self)
-
-	self.CreateClock = CreateClock
-	self.CreateCoords = CreateCoords
 end
 
 
